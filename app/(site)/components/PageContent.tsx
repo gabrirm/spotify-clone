efde3662a59962fe8ac9f7fe2b08e97774cd3487
gabrirm <gabrirm00@gmail.com>
2023-11-21 23:01:43 +0100

    implemented error and loading handling

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -3,13 +3,13 @@
 import SongItem from "@/components/SongItem";
 import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
-
 interface PageContentProps {
   songs: Song[];
 }
 
 const PageContent: React.FC<PageContentProps> = ({ songs }) => {
-  const onPlay = useOnPlay(songs)
+  
+  const onPlay = useOnPlay(songs);
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available</div>;
   }
@@ -28,7 +28,11 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
   "
     >
       {songs.map((item) => (
-        <SongItem key={item.id} onClick={(id: string) => onPlay(id)} data={item} />
+        <SongItem
+          key={item.id}
+          onClick={(id: string) => onPlay(id)}
+          data={item}
+        />
       ))}
     </div>
   );
